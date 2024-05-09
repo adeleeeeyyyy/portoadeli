42cf43eeb8db7b50e2e1b1541aa861798f43ca54
Mitsuhamiyamizu213 <mitsuhaadly@gmail.com>
2024-05-09 12:32:16 +0700

    cut contact

--- assets/js/email.js
+++ assets/js/email.js
@@ -5,12 +5,5 @@ function sendMail(){
     message : document.getElementById("message").value,
   }
   
-  emailjs.send('service_2enydma', 'template_1jjpxd1', parms).then(
-  (response) => {
-    console.log('SUCCESS!', response.status, response.text);
-  },
-  (error) => {
-    console.log('FAILED...', error);
-  },
-);
+  emailjs.send('service_2enydma', 'template_1jjpxd1', parms).then(alert("success"))
 }
