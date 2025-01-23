25ec1dd70ece17867f5643da55aff890bfae75a5
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-01-23 20:24:34 +0700

    rapih rapih kode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const lenis = new Lenis();
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+});
+
 'use strict';
 
 
